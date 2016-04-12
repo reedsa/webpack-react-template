41c4acaa64218e242e527754c8a64f2def74406b
Stuart Reed <stuart.reed@colorado.edu>
2016-04-11 21:55:24 -0600

    Refactor paths in webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,14 +1,20 @@
 import webpack from 'webpack';
+import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+var paths = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
 
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    __dirname + '/app/main.js'
+    paths.app
   ],
   output: {
-    path: __dirname + '/build',
+    path: paths.build,
     filename: 'bundle.js'
   },
   plugins: [
@@ -23,19 +29,19 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'eslint',
-        include: __dirname + '/app'
+        include: paths.app
       }
     ],
     loaders : [
       {
         test: /\.js$/,
-        include: __dirname + '/app',
-        loader: 'react-hot'
+        loader: 'react-hot',
+        include: paths.app
       },
       {
         test : /\.js$/,
-        include : __dirname + '/app',
         loader : 'babel-loader',
+        include: paths.app,
         query: {
           presets: ['es2016', 'react']
         }
