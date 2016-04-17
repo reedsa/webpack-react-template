8335caa4be227e8b657af1b2f5aed9ac519a0c85
Stuart Reed <stuart.reed@colorado.edu>
2016-04-17 14:13:27 -0600

    Generate index and import css

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,50 +5,59 @@ const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const TARGET = process.env.npm_lifecycle_event;
-process.env.BABEL_ENV = TARGET;
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
+  style: path.join(__dirname, 'app/stylesheets/main.css')
 };
+
+process.env.BABEL_ENV = TARGET;
 
 const common = {
   entry: {
-    app: PATHS.app
+    app: PATHS.app,
+    style: PATHS.style
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module : {
     preLoaders: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         loader: 'eslint',
         include: PATHS.app
       }
     ],
-    loaders : [
+    loaders: [
       {
-        test : /\.jsx?$/,
-        loader : 'babel?cacheDirectory',
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
         include: PATHS.app
       }
     ]
-  }
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'node_modules/html-webpack-template/index.ejs',
+      title: 'Webpack React Example',
+      appMountId: 'app',
+      inject: false
+    })
+  ]
+};
 
 // Default configuration
 if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
+    devtool: 'eval-source-map',
     devServer: {
       contentBase: PATHS.build,
-
-      // Enable sourcemaps
-      devtool: 'eval-source-map',
 
       // Enable history API fallback so HTML5 History API based
       // routing works.
@@ -64,20 +73,25 @@ if(TARGET === 'start' || !TARGET) {
       host: process.env.HOST,
       port: process.env.PORT || 3000
     },
+    module: {
+      loaders: [
+        // Define development specific CSS setup
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: PATHS.app
+        }
+      ]
+    },
     plugins: [
-      // Generate index.html from the template file
-      new HtmlWebpackPlugin({
-        template: 'app/index.tpl.html',
-        filename: 'index.html'
-      }),
       new webpack.HotModuleReplacementPlugin(),
       new NpmInstallPlugin({
         save: true // --save
       })
     ]
   });
-}
+};
 
 if(TARGET === 'build') {
   module.exports = merge(common, {});
-}
+};
